195437fd5483fedcfa0d13ebc74cb7ae60c9ff58
CarefreeSlacker <struan.dirk.29@gmail.com>
2016-05-05 00:04:44 +0300

    feature/add_posts state machine has been added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,9 @@ class Post < ActiveRecord::Base
 	validates :name , length: { minimum: 5 }
 	validates :text, length: { minimum: 1, maximum: 300 }
 
+	after_initialize :set_initial_state
+
 	state_machine :published_state, initial: :draught do
-		state :draught
 		state :published
 		state :delayed_publishing
 
@@ -14,5 +15,13 @@ class Post < ActiveRecord::Base
 		event :delay_publish do
 			transition draught: :delayed_publishing
 		end
+	end
+
+	self.per_page = 3 #TODO change this to 20 after the debug
+
+	private
+
+	def set_initial_state
+		self.published_state ||= :draught
 	end
 end
---
+++ app/service/post_creator_service.rb
@@ -0,0 +1,37 @@
+class PostCreatorService
+	include ActiveModel::Validations
+
+	attr_reader :post
+
+	validate :validate_post
+
+	def initialize(action, post_params)
+		@post = Post.new(post_params)
+		@post.published_state = set_published_state(action)
+	end
+
+	def submit
+		if valid?
+			@post.save
+		else
+			false
+		end
+	end
+
+	private
+
+	def set_published_state(action)
+		case action
+		when 'Publish' then :published
+		when 'Save draught' then :draught
+		when 'Delay published' then :delayed_publishing
+		else :draught
+		end
+	end
+
+	def validate_post
+		unless @post.valid?
+			self.errors.add(:base, @post.errors)
+		end
+	end
+end
